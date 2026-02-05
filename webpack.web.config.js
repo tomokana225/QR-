f0d5e46fbca947def70cdc4fcefd666f95cd1518
tomokana2525 <tomoyaking0225@gmail.com>
2026-02-05 20:25:21 +0900

    feat: Add web build configuration
    Configure Webpack for building a web version of the application. This includes setting up HTML generation, CSS extraction, and asset copying.

---
+++ webpack.web.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/renderer.tsx',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: 'public', 
+          to: '.',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+          noErrorOnMissing: true
+        }
+      ]
+    }),
+  ],
+};
